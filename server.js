629219b46f079df42daa6c4c9fd056e7f356ed67
Hu <huang.h0709@gmail.com>
2023-08-08 21:02:55 -0400

    finished Greetings

---
+++ server.js
@@ -0,0 +1,19 @@
+const express=require("express")
+const app= express()
+
+app.set("view engine","jsx")
+app.engine("jsx",require("express-react-views").createEngine())
+
+// app.get('/greeting',(req,res)=>{
+//     res.send("<h1>Hello, stranger</h1>")
+// })
+app.get('/greeting/:name',(req,res)=>{
+    res.send(`Hello, ${req.params.name}`)
+})
+
+
+
+
+app.listen(3000,(req,res)=>{
+    console.log("Listening...")
+})
