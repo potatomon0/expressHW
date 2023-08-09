f65a9d91404b175dda3080b84453b936a2d95125
Hu <huang.h0709@gmail.com>
2023-08-08 21:52:31 -0400

    Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -17,7 +17,13 @@ app.get('/tip',(req,res)=>{
     console.log(newTip)
     res.send(`${tipAmount}`)
 })
-
+const ansArray = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+app.get('/magic/:question',(req,res)=>{
+    let question = req.params.question
+    let len = ansArray.length-1
+    let answer = Math.floor(Math.random()*len)
+    res.send(`${question} <h1>${ansArray[answer]}</h1>`)
+})
 
 
 app.listen(3000,(req,res)=>{
